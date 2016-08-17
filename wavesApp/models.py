743da3e7ca174304e044f1aa154db55f14714693
Gene S <eshuster@kontrolscan.com>
2016-08-17 15:45:47 -0700

    Add views and methods to models; Add a few templates

--- wavesApp/models.py
+++ wavesApp/models.py
@@ -1,14 +1,26 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 
 class Genre(models.Model):
-    genre_text = models.CharField(max_length=200)
-    choice_text = models.CharField(max_length=200)
+  genre_text = models.CharField(max_length=200)
+  choice_text = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.choice_text
 
 class Song(models.Model):
-    song_text = models.CharField(max_length=200)
-    date_added = models.DateTimeField('date added')
-    likes = models.IntegerField(default=0)
-    song_genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+  song_text = models.CharField(max_length=200)
+  date_added = models.DateTimeField('date added')
+  likes = models.IntegerField(default=0)
+  song_genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+  def was_added_recently(self):
+    return self.date_added >= timezone.now() - datetime.timedelta(days=1)
+
+  def __str__(self):
+    return self.song_text
--- wavesApp/urls.py
+++ wavesApp/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url
-
 from . import views
 
+app_name = 'wavesApp'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+  # ex: /wavesApp/
+  url(r'^$', views.index, name='index'),
+  # ex: /wavesApp/5/
+  url(r'^(?P<song_id>[0-9]+)/$', views.detail, name='detail'),
+  # ex: /wavesApp/5/results/
+  url(r'^(?P<song_id>[0-9]+)/results/$', views.results, name='results'),
+  # ex: /wavesApp/5/vote/
+  url(r'^(?P<song_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
